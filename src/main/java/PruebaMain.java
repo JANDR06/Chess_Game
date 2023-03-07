ce1fb272b4deaf35fd3c54809df1cc4426ea6d58
ajorgep <alejorpas2@alu.edu.gva.es>
2023-03-07 20:57:15 +0100

    Commit

---
+++ src/main/java/PruebaMain.java
@@ -0,0 +1,12 @@
+import model.*;
+import view.Screen;
+
+public class PruebaMain {
+
+    public static void main(String[] args) {
+        Board b = new Board();
+        b.placePieces();
+        Screen.showBoard(b);
+
+    }
+}
